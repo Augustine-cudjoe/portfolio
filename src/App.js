bb8b59baa82583ba2a67b0ddccbab7f661e117b5
Augustine Cudjoe <auguscudjoe94@gmail.com>
2023-03-01 14:19:15 +0100

    my portfolio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> boot strap</h1>
+     
+    </div>
+  );
+}
+
+export default App;
+    
